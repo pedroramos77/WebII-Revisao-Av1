490e69babe0548b30abb64a3f2aba4bf16d15d5e
pedroramos77 <pedrogabrielguimaraesramos178@gmail.com>
2024-10-16 15:28:00 -0300

    commit two

---
+++ script.js
@@ -0,0 +1,10 @@
+// num = 20
+
+// document.write(`${num} - 2 = ${num -2}`)
+
+// ----------------------
+
+// a1 = +(prompt("Digite um numero"))
+// b1 = +(prompt('Digite um numero'))
+
+// alert(`A soma de ${a1} e ${b1} é ${a1 + b1}`)
