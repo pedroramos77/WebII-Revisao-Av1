971c2b9f20e11e89b9b465143f72f7b26659ab2c
pedroramos77 <pedrogabrielguimaraesramos178@gmail.com>
2024-10-16 16:09:28 -0300

    OPERADORES

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// REV AV1 WEB2
+
+// OPERADORES
+
+
 // num = 20
 
 // document.write(`${num} - 2 = ${num -2}`)
@@ -8,3 +13,55 @@
 // b1 = +(prompt('Digite um numero'))
 
 // alert(`A soma de ${a1} e ${b1} é ${a1 + b1}`)
+// document.write(`A soma de ${a1} e ${b1} é ${a1 + b1}`)
+// console.log(`A soma de ${a1} e ${b1} é ${a1 + b1}`)
+
+// console.log('a soma dos valores ' + a1, 'e ' + b1, 'é = ' + (a1 + b1))
+
+// --------------------------------------
+
+// num2 = 10;
+
+// document.write(' '+ num2--)
+// document.write(' ' + num2)
+
+// var x = 10
+// var y = '10'
+
+// document.write(`<br>O valor ${x} é igual ${y}? ${x == y}`)
+// document.write(`<br>O valor ${x} é igual identico ${y}? ${x === y}`)
+
+// === -> identico
+// == -> igual
+
+// document.write(`O valor de ${x} é diferente de ${y}? ${x != y}`)
+// document.write(`<br>O valor de ${x} é diferente de ${y}? ${x !== y}`)
+
+// ----------------------------------
+
+// var n1 = +(prompt('Digite um numero'))
+// var n2 = parseFloat(prompt('DIgite um numero'))
+
+
+// document.write(`<br>O valor de ${n1} > ${n2}? ${n1 > n2}`)
+// document.write(`<br>O valor de ${n1} >= ${n2}? ${n1 >= n2}`)
+// document.write(`<br>O valor de ${n1} < ${n2}? ${n1 < n2}`)
+// document.write(`<br>O valor de ${n1} <= ${n2}? ${n1 <= n2}`)
+
+// ------------------------------
+
+// console.log(20 < 40 && 30 > 5) //true
+// console.log(20 < 40 && 30 < 5) //false
+// console.log(20 < 40 || 30 < 5) //true
+// console.log(2 < 3) // true
+// console.log(!(2 < 3)) // false
+// console.log(!!(2 < 3)) // true
+
+// -----------------------------------------
+
+// BIBLIOTECA MATH
+
+
+
+
+
