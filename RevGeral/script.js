e376107f5bce7c2d81473fd992643e8a925450f3
pedroramos77 <pedrogabrielguimaraesramos178@gmail.com>
2024-10-16 22:33:39 -0300

    up

--- RevGeral/script.js
+++ RevGeral/script.js
@@ -480,12 +480,212 @@
 // // document.write(`<br> ${b[5][2]}`)
 // // document.write(`<br>${b[5]}`)
 
+// tamamho-----------
+
 // document.write(`${b.length}`)
-// document.write(`${b[5].length}`)
+// document.write(`${b[5].length}`) 
 
 
+// indentifica posição
+
+// document.write(`<br>A posição do elemento a encontra-se na posição : ${b.indexOf('a')}`)
+
+// ultima ocorrencia 
+
+// document.write(`<br>A ultima occotência do elemento é: ${b.lastIndexOf('2')}`)
+
+// modo literal ----------
+
+// var c = []
+// document.write(`<br> ${c}`)
+// document.write(`<br>O tamanho do array é ${c.length}`)
+
+// var d = [true,1,2,3,4,[1,2,3],2,'a']
+// document.write(`<br> ${d}`)
+// document.write(`<br> ${d[0]}`)
+// document.write(`<br> ${d[5][0]}`)
+
+// -------------
 
 
+// valores externos------------
 
 
+// nota = []
+
+// for(i = 0; i < 4;i++){
+// 	nota[i] = +(prompt('Digite um numero')) 
+// 	// nota[i] = i + 1 
+	
+	
+// }
+
+// document.write(`${nota}`)
+
+
+// string ---------------------------
+
+
+// nome = 'Thereza'
+
+// document.write(`o nome tem ${nome.length} caracteres`)
+
+// var e = [1,2,3,4]
+
+// e[e.length] = 'Novo Valor'
+
+// document.write(`<br>${e}`)
+
+
+// pessoa = ['Ana', 'Liz', 'Eva', 'Flora', 'Isabel']
+
+// for(i = 0; i < pessoa.length;i++){
+// 	document.write(`<br> ${pessoa[i]}`)
+// }
+
+
+// Ordenação ------------------------
+
+// reverse() -> ordem inversa
+
+// pessoa = ['Ana','Liz','Eva','Flora','Isabel']
+// document.write(`<br>${pessoa}`)
+// document.write(`<br>A ordem invertida do array é : ${pessoa.reverse()}`)
+
+// // sort() -> ordem alfabetica
+
+// document.write(`<br>A ordem alfabetica do array é: ${pessoa.sort()}`)
+
+// document.write(`<br>A ordem alfabetica do array é: ${pessoa.sort().reverse()}`)
+
+// concatenação---------
+
+// var f = [1,2,3,5]
+// var g = [6,7,8]
+
+// document.write(`<br>O vetor concatenado é :${f.concat(g)}`)
+// document.write(`<br>O vetor concatenado é :${g.concat(f)}`)
+
+//includes() -> verifica se um elemento encontras-se no array !!!!!!!! diferente do indexOf ou lastIndexOf
+
+// var f = [1,2,3,5]
+// var g = [6,7,8]
+
+
+// document.write(`<br>O numero 2 encontra-se no array? ${f.includes(2)}`)
+// document.write(`<br>O numero 12 encontra-se no array? ${f.includes(12)}`)
+
+// push() -> elemento no final !!!!!!!!!
+
+// var h = [10, 20, 30, 40]
+
+// h.push(50)
+
+// document.write(`<br> ${h}`)
+
+// pop() -> remove o ultimo !!!!!!
+
+// h.pop()
+
+// document.write(`<br> ${h}`)
+
+// unshift() -> adiciona no inicio
+
+// h.unshift(0)
+
+// document.write(`<br>O novo array é: ${h}`)
+
+// shift() -> remove do inicio
+
+// h.shift()
+
+// document.write(`<br>O novo array é: ${h}`)
+
+// slice() -> fatia a array sem alterar o original
+
+// aluno = 'Maria da Silva Xavier da Cunha'
+// a = aluno.slice(5)
+// document.write(`<br>${a}`)
+// a1 = aluno.slice(5,14)
+// document.write(`<br>${a1}`)
+
+
+// carro = ['Jeta','Fiat','Focus','Cruze','Corolla','Civic','Elantra']
+// c = carro.slice(2)
+// document.write(`<br>${c}`)
+
+// c1 = carro.slice(2,3)
+// document.write(`<br>${c1}`)
+
+// c2 = carro.slice(2,5)
+// document.write(`<br>${c2}`)
+
+// c3 = carro.slice(-4,-2)
+// document.write(`<br>${c3}`)
+
+// c4 = carro.slice(-6,-3)
+// document.write(`<br>${c4}`)
+
+// email = 'exemplo@gmail.com'
+// e = email.slice(email.indexOf('@') + 1)
+// document.write(`<br>${e}`)
+
+// e1 = email.slice(email.lastIndexOf('e') + 1)
+// document.write(`<br>${e1}`)
+
+// e1 = email.slice(email.lastIndexOf('e'))
+// document.write(`<br>${e1}`)
+
+// nome1 = 'Thereza'
+// n1 = nome1.slice(-3)
+// document.write(`<br>${n1}`)
+
+
+// splice() - 1º valor: posição
+			//  2º valor: quantidade de elementos excluidos
+			//  3º valor: novos elementos 
+			 
+// d = ['Segunda','Terça','Quarta']
+// d1 = d.splice(1,1)
+// document.write(`<br>${d1}`)
+// document.write(`<br>${d}`)
+
+// d2 = d.splice(1,1,'Quinta','Sexta')
+// document.write(`<br>${d2}`)
+// document.write(`<br>${d}`)
+
+// d3 = d.splice(1,0,'Quinta','Sexta')
+// document.write(`<br>${d3}`)
+// document.write(`<br>${d}`)
+
+// nome = []
+// for(i=0;i<5;i++){
+// 	nome[i] = prompt('Digite o seu nome')
+// }
+// document.write(`<br>Nome: ${nome}`)
+// n = nome.splice(2,0,'1')  <----------------------------!!!!!!!!!!!
+// document.write(`<br>Nome: ${nome}`)
+
+
+// FUNÇÃO DE DNV-----------------
+
+// /Função anonima - não possui nome, atribui-se a função a uma variavel
+
+// var msg = function(){
+// 	return `Olá!Bom dia!`
+// }
+// document.write(`<br>${msg()}`)
+// //---------------------------------------
+// var msg1 = function(){
+// 	return `Olá ${nome}!Bom dia!`
+// }
+// nome = prompt('Digite o seu nome')
+// document.write(`<br>${msg1()}`)
+// //---------------------------------------
+
+// num = function(numero){
+// 	return numero ** 2
+// }
+// numero = +(prompt('Digite um numero'))
+// document.write(`<br>${num(numero)}`)
 
